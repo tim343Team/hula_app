e77c7d009c5782b96c1ef77399a7516c1f724c62
“timqi” <liuweiqitim@gmail.com>
2022-09-03 15:57:44 +0800

    更新代码

--- app/src/main/java/com/hula/myapplication/view/mine/MineFragment.java
+++ app/src/main/java/com/hula/myapplication/view/mine/MineFragment.java
@@ -1,41 +1,15 @@
 package com.hula.myapplication.view.mine;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 
 import com.hula.myapplication.R;
-import com.hula.myapplication.adapter.PagerAdapter;
-import com.hula.myapplication.view.mine.sub_fragment.EditFragment;
-import com.hula.myapplication.view.mine.sub_fragment.PreviewFragment;
-import com.hula.myapplication.widget.NoScrollViewPager;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.hula.myapplication.view.mine.profile.ProfileActivity;
 
 import tim.com.libnetwork.base.BaseTransFragment;
 
 public class MineFragment extends BaseTransFragment {
     public static final String TAG = MineFragment.class.getSimpleName();
-    TextView[] tvTabs;
-    NoScrollViewPager viewpager;
-    private PagerAdapter adapter;
-    private PreviewFragment subFragment1;
-    private EditFragment subFragment2;
-    private ArrayList<String> tabs = new ArrayList<>();
-    protected List<Fragment> fragments = new ArrayList<>();
-
-    private TextView tvPreview;
-    private TextView tvEdit;
-
 
     @Override
     protected String getmTag() {
@@ -59,12 +33,48 @@ public class MineFragment extends BaseTransFragment {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
-        viewpager = rootView.findViewById(R.id.viewpager_mine);
-        tvPreview = rootView.findViewById(R.id.tv_preview);
-        tvEdit = rootView.findViewById(R.id.tv_edit);
-        tvTabs = new TextView[]{tvPreview, tvEdit};
-        setView();
-        showTab(1);
+        rootView.findViewById(R.id.ll_profile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProfileActivity.actionStart(getmActivity());
+            }
+        });
+        rootView.findViewById(R.id.ll_preference).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rootView.findViewById(R.id.ll_invite_friend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rootView.findViewById(R.id.ll_account_privacy).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rootView.findViewById(R.id.ll_what_new).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rootView.findViewById(R.id.ll_help_center).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rootView.findViewById(R.id.ll_log_out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
@@ -74,18 +84,7 @@ public class MineFragment extends BaseTransFragment {
 
     @Override
     protected void fillWidget() {
-        tvPreview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showTab(0);
-            }
-        });
-        tvEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showTab(1);
-            }
-        });
+
     }
 
     @Override
@@ -103,33 +102,5 @@ public class MineFragment extends BaseTransFragment {
 
     }
 
-    private void setView() {
-        tabs.clear();
-        fragments.clear();
-        for (TextView textView : tvTabs) {
-            tabs.add("");
-        }
-        fragments.add(subFragment1 = PreviewFragment.getInstance());
-        fragments.add(subFragment2 = EditFragment.getInstance());
-        if (adapter == null) {
-            adapter = new PagerAdapter(getActivity().getSupportFragmentManager(), fragments, tabs);
-            viewpager.setAdapter(adapter);
-            viewpager.setOffscreenPageLimit(fragments.size() - 1);
-            viewpager.setCurrentItem(1);
-        } else {
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-    void showTab(int position) {
-        for (int i = 0; i < tvTabs.length; i++) {
-            if (i == position) {
-                tvTabs[i].setSelected(true);
-            } else {
-                tvTabs[i].setSelected(false);
-            }
-        }
-        viewpager.setCurrentItem(position);
-    }
 
 }
---
+++ app/src/main/java/com/hula/myapplication/view/mine/profile/ProfileActivity.java
@@ -0,0 +1,113 @@
+package com.hula.myapplication.view.mine.profile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.hula.myapplication.adapter.PagerAdapter;
+import com.hula.myapplication.databinding.ActivityProfileBinding;
+import com.hula.myapplication.widget.NoScrollViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tim.com.libnetwork.base.BaseActivity;
+
+public class ProfileActivity extends BaseActivity {
+    private ActivityProfileBinding binding;
+    TextView[] tvTabs;
+    NoScrollViewPager viewpager;
+    private PagerAdapter adapter;
+    private PreviewFragment subFragment1;
+    private EditFragment subFragment2;
+    private ArrayList<String> tabs = new ArrayList<>();
+    protected List<Fragment> fragments = new ArrayList<>();
+
+    private TextView tvPreview;
+    private TextView tvEdit;
+
+    public static void actionStart(Activity activity) {
+        Intent intent = new Intent(activity, ProfileActivity.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected int getActivityLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected View getActivityLayoutView() {
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        viewpager = binding.viewpagerMine;
+        tvPreview = binding.tvPreview;
+        tvEdit = binding.tvEdit;
+        tvTabs = new TextView[]{tvPreview, tvEdit};
+        setView();
+        showTab(1);
+    }
+
+    @Override
+    protected void obtainData() {
+
+    }
+
+    @Override
+    protected void fillWidget() {
+        tvPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTab(0);
+            }
+        });
+        tvEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTab(1);
+            }
+        });
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    private void setView() {
+        tabs.clear();
+        fragments.clear();
+        for (TextView textView : tvTabs) {
+            tabs.add("");
+        }
+        fragments.add(subFragment1 = PreviewFragment.getInstance());
+        fragments.add(subFragment2 = EditFragment.getInstance());
+        if (adapter == null) {
+            adapter = new PagerAdapter(getSupportFragmentManager(), fragments, tabs);
+            viewpager.setAdapter(adapter);
+            viewpager.setOffscreenPageLimit(fragments.size() - 1);
+            viewpager.setCurrentItem(1);
+        } else {
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    void showTab(int position) {
+        for (int i = 0; i < tvTabs.length; i++) {
+            if (i == position) {
+                tvTabs[i].setSelected(true);
+            } else {
+                tvTabs[i].setSelected(false);
+            }
+        }
+        viewpager.setCurrentItem(position);
+    }
+}
