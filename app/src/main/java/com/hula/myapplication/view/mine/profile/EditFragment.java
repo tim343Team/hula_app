b52a04ff45180ea5045cc76a7ce189303e715c71
“timqi” <liuweiqitim@gmail.com>
2022-09-06 15:10:20 +0800

    更新代码

--- app/src/main/java/com/hula/myapplication/view/mine/profile/EditFragment.java
+++ app/src/main/java/com/hula/myapplication/view/mine/profile/EditFragment.java
@@ -9,7 +9,18 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hula.myapplication.R;
+import com.hula.myapplication.adapter.CategoriesAdapter;
+import com.hula.myapplication.adapter.NewAdapter;
+import com.hula.myapplication.dao.SubCategoriesDao;
 import com.hula.myapplication.databinding.FragmentMineEditBinding;
+import com.library.flowlayout.FlowLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import tim.com.libnetwork.base.BaseLazyFragment;
 
@@ -18,6 +29,10 @@ public class EditFragment extends BaseLazyFragment {
     private View llAboutMe;
     private EditText editAboutMe;
     private TextView tvEditLength;
+    private RecyclerView recyclerProfile;
+    private RecyclerView recyclerCategorie;
+    private CategoriesAdapter categoriesAdapter;
+    private List<SubCategoriesDao> subCategoriesDaos=new ArrayList<>();
 
     public static EditFragment getInstance() {
         EditFragment fragment = new EditFragment();
@@ -47,6 +62,8 @@ public class EditFragment extends BaseLazyFragment {
         llAboutMe = binding.llAboutMe;
         editAboutMe = binding.editAboutMe;
         tvEditLength = binding.tvEditLength;
+        recyclerProfile = binding.recyclerProfile;
+        recyclerCategorie = binding.recyclerCategorie;
         llAboutMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,6 +96,8 @@ public class EditFragment extends BaseLazyFragment {
                 tvEditLength.setText((160 - content.length()) + "");
             }
         });
+        initRecyclerViewProfile();
+        initRecyclerViewCategorie();
     }
 
     @Override
@@ -104,5 +123,20 @@ public class EditFragment extends BaseLazyFragment {
     @Override
     protected void initDestroy() {
 
+    }
+
+    private void initRecyclerViewProfile() {
+//        recyclerProfile.setLayoutManager(new FlowLayoutManager());
+//        adapter = new NewAdapter(R.layout.adapter_what_new, data);
+//        adapter.bindToRecyclerView(recyclerView);
+//        adapter.setEnableLoadMore(false);
+    }
+
+    private void initRecyclerViewCategorie() {
+        subCategoriesDaos.add(new SubCategoriesDao());
+        recyclerCategorie.setLayoutManager(new FlowLayoutManager());
+        categoriesAdapter = new CategoriesAdapter(R.layout.adapter_categorie, subCategoriesDaos);
+        categoriesAdapter.bindToRecyclerView(recyclerCategorie);
+        categoriesAdapter.setEnableLoadMore(false);
     }
 }
